6c04835b19a36d0f0bf88cee627a677668845397
seo-dongyeon <s.dongyeon.me@gmail.com>
2026-09-19 22:28:53 +0900

    solve 05_null_return_deref

--- challenges/05_null_return_deref/bug.c
+++ challenges/05_null_return_deref/bug.c
@@ -63,10 +63,13 @@ static void expand(const Config *c, const char *tmpl, char *out, size_t outcap) 
             if (kl >= sizeof key) kl = sizeof key - 1;
             memcpy(key, p + 2, kl);
             key[kl] = '\0';
-
-            const char *v = cfg_get(c, key);      
-            size_t vl = strlen(v);                 
-            if (o + vl < outcap) { memcpy(out + o, v, vl); o += vl; }
+            
+            const char *v = cfg_get(c, key);     
+            if (v != NULL)
+            {
+                size_t vl = strlen(v);                 
+                if (o + vl < outcap) { memcpy(out + o, v, vl); o += vl; }
+            }
             p = end + 1;
         } else {
             if (o + 1 < outcap) out[o++] = *p;
